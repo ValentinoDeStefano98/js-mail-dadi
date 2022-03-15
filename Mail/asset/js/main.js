b0b02b0c63f5651a6950f78c79b61dfaa2d6a7b4
Valentino De Stefano <valentino.destefano46@gmail.com>
2022-03-15 17:15:07 +0100

    Some style added

--- Mail/asset/js/main.js
+++ Mail/asset/js/main.js
@@ -2,12 +2,17 @@
 let mail = ["valentino.destefano46@gmail.com", "pincopallo@hotmail.com", "gino.libero@gmail.com"]
 console.log(mail);
 
-let mailPersonale = prompt("Inserisci la tua mail:")
+let mailPersonale = prompt("Inserisci la tua mail:");
+let mailTrovata = false;
 
 for (let i=0; i < mail.length; i++){
     if ( mailPersonale == mail[i]){
+        mailTrovata = true;
+    }
+}
+
+if ( mailTrovata == true){
         console.log("sei già registrato");
     } else{
         console.log("non risulti registrato, corri a farlo!")
     }
-}
