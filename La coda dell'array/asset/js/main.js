06b89c6003564105c5ac411187ca2db0846cd6ab
Valentino De Stefano <valentino.destefano46@gmail.com>
2022-03-15 15:47:18 +0100

    3rd bonus completed

--- La coda dell'array/asset/js/main.js
+++ La coda dell'array/asset/js/main.js
@@ -1,15 +1,18 @@
+// Primo esercizio bonus
 let array = [1, 3, 5, 7, 6, 8, 5, 3, 4, 5, 6, 1];
 
 for ( i = array.length - 5; i < array.length; i++){
     console.log(array[i]);
 }
 
+// Secondo esercizio bonus
 let numeroElementi = prompt("Quanti elementi deve contenere la lista?");
+
 if (numeroElementi <= 0){
     alert("Attenzione! Inserisci un valore corretto!");
 }
-let arrayVuoto = [];
 
+let arrayVuoto = [];
 
 for (let i = 0; i < numeroElementi; i++){
     let elementoCasuale = Math.floor(Math.random()*99999);
@@ -17,3 +20,10 @@ for (let i = 0; i < numeroElementi; i++){
 }   
 
 console.log(arrayVuoto);
+
+// Terzo esercizio bonus
+let numeroEstratti = prompt("Quanti elementi vuoi estrarre dal fondo della lista?");
+
+for (i = (arrayVuoto.length - numeroEstratti); i < arrayVuoto.length; i++){
+    console.log(arrayVuoto[i]);
+}
