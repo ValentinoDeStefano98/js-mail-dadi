6645d5e4c8715951e67a70ada5e69efdb2f0b31f
Valentino De Stefano <valentino.destefano46@gmail.com>
2022-03-15 17:34:05 +0100

    Buttons added

--- Gioco dei dadi/asset/js/main.js
+++ Gioco dei dadi/asset/js/main.js
@@ -1,13 +1,39 @@
-let randomPc = Math.floor(Math.random()*6 + 1);
-console.log(randomPc);
+let randomPc;
+let randomUtente;
 
-let randomUtente = Math.floor(Math.random()*6 + 1);
-console.log(randomUtente);
+let buttonPc = document.getElementById("guestNumber");
 
-if (randomPc > randomUtente){
-    console.log("Ha vinto il PC!")
-} else if(randomPc < randomUtente){
-    console.log("Ha vinto l'utente!")
-} else{
-    console.log("La partita è finita in parità!")
-}
+buttonPc.addEventListener("click",
+    function(){
+    randomPc = Math.floor(Math.random()*6 + 1);
+    }
+);
+
+let buttonUtente = document.getElementById("yourNumber");
+
+buttonUtente.addEventListener("click",
+    function(){
+    randomUtente = Math.floor(Math.random()*6 + 1);
+    }
+);
+
+let buttonResult = document.getElementById("result");
+
+buttonResult.addEventListener("click",
+    function(){
+        if (randomPc == null || randomUtente == null){
+            alert("Attenzione! Genera prima i numeri!");
+            return;
+        }
+        document.getElementById("numberUtente").innerHTML = `Il tuo numero è: ${randomUtente}`;
+        document.getElementById("numberPC").innerHTML = `Il numero del tuo avversario è: ${randomPc}`;
+        if (randomPc > randomUtente){
+            document.getElementById("score").innerHTML = "Ha vinto il PC!";
+        } else if(randomPc < randomUtente){
+            document.getElementById("score").innerHTML = "Hai vinto!";
+        } else{
+            document.getElementById("score").innerHTML = "Parità!";
+        }
+    }
+);
+
