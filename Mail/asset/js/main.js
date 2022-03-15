23435bc7be9e460c6314f2f3c1d7a3b5a40f51af
Valentino De Stefano <valentino.destefano46@gmail.com>
2022-03-15 20:52:35 +0100

    Effects added

--- Mail/asset/js/main.js
+++ Mail/asset/js/main.js
@@ -2,17 +2,34 @@
 let mail = ["valentino.destefano46@gmail.com", "pincopallo@hotmail.com", "gino.libero@gmail.com"]
 console.log(mail);
 
-let mailPersonale = prompt("Inserisci la tua mail:");
-let mailTrovata = false;
+let button = document.getElementById("mailButton");
 
-for (let i=0; i < mail.length; i++){
-    if ( mailPersonale == mail[i]){
-        mailTrovata = true;
-    }
-}
+let form = document.getElementById("mailForm");
 
-if ( mailTrovata == true){
-        console.log("sei già registrato");
-    } else{
-        console.log("non risulti registrato, corri a farlo!")
+button.addEventListener("click",
+    function(){
+        let mailPersonale = prompt("Inserisci la tua e-mail:");
+        if ( mailPersonale.length == 0){
+                alert("Attenzione! Inserisci correttamente la tua e-mail!");
+                return;
+            }
+        let mailTrovata = false;
+
+        for (let i=0; i < mail.length; i++){
+            if ( mailPersonale == mail[i]){
+                mailTrovata = true;
+            }
+        }
+
+        if ( mailTrovata == true){
+                document.getElementById("mailCamp").innerHTML = `<div class="p-2 border rounded-2">${mailPersonale}</div>`;
+                form.classList.remove("d-none");
+        } else{
+                alert("Non risulti registrato, corri a farlo!");
+                return;
+        }
     }
+);
+
+
+
