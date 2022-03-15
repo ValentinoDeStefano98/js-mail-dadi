898f0ccafad2737dc12a9a2bd8c6482c3da57d72
Valentino De Stefano <valentino.destefano46@gmail.com>
2022-03-15 17:03:19 +0100

    Buttons added

--- La coda dell'array/asset/js/main.js
+++ La coda dell'array/asset/js/main.js
@@ -1,29 +1,46 @@
 // Primo esercizio bonus
 let array = [1, 3, 5, 7, 6, 8, 5, 3, 4, 5, 6, 1];
 
-for ( i = array.length - 5; i < array.length; i++){
-    console.log(array[i]);
+for (i = 0; i < array.length; i++) {
+    document.getElementById("initialArray").innerHTML += `<div class="p-3">${array[i]}</div>`;
+}
+
+for (i = array.length - 5; i < array.length; i++) {
+    document.getElementById("finalArray").innerHTML += `<div class="p-3">${array[i]}</div>`;
 }
 
 // Secondo esercizio bonus
-let numeroElementi = prompt("Quanti elementi deve contenere la lista?");
-
-if (numeroElementi <= 0){
-    alert("Attenzione! Inserisci un valore corretto!");
-}
+let buttonArray = document.getElementById("arrayGenerator");
 
 let arrayVuoto = [];
 
-for (let i = 0; i < numeroElementi; i++){
-    let elementoCasuale = Math.floor(Math.random()*99999);
-    arrayVuoto[i] = elementoCasuale;
-}   
-
-console.log(arrayVuoto);
+buttonArray.addEventListener("click",
+    function () {
+        let numeroElementi = prompt("Quanti elementi deve contenere la lista?");
+        if (numeroElementi <= 0) {
+            alert("Attenzione! Inserisci un valore corretto!");
+        }
+        for (let i = 0; i < numeroElementi; i++) {
+            let elementoCasuale = Math.floor(Math.random() * 99999);
+            arrayVuoto[i] = elementoCasuale;
+            document.getElementById("secondArray").innerHTML += `<div class="p-3">${arrayVuoto[i]}</div>`;
+        }
+        
+    }
+);
 
 // Terzo esercizio bonus
-let numeroEstratti = prompt("Quanti elementi vuoi estrarre dal fondo della lista?");
+let buttonElements = document.getElementById("elementExtract");
 
-for (i = (arrayVuoto.length - numeroEstratti); i < arrayVuoto.length; i++){
-    console.log(arrayVuoto[i]);
-}
+buttonElements.addEventListener("click",
+    function () {
+        if (arrayVuoto.length == 0) {
+            alert("Attenzione, inserisci prima i valori per creare l'array");
+            return;
+        }
+        let numeroEstratti = prompt("Quanti elementi vuoi estrarre dal fondo della lista?");
+        for (i = (arrayVuoto.length - numeroEstratti); i < arrayVuoto.length; i++) {
+            document.getElementById("thirdArray").innerHTML += `<div class="p-3">${arrayVuoto[i]}</div>`;
+        }
+    }
+);
